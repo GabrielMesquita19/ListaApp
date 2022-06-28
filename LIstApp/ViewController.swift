93e60c2d719074e90d536ee065b0002b21ea992a
gabriel mesquita <mesquitagabriel19@icloud.com>
2022-06-28 00:57:07 -0300

    Initial Commit

---
+++ LIstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LIstApp
+//
+//  Created by User on 28/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
